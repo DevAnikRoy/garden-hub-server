78acf6f47f157f10fe44fa5fba2747c319046561
DevAnikRoy <anikroy302@gmail.com>
2025-05-20 15:17:44 +0600

    successfully added the server and the mongodb and other things

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+const app = express()
 const port = process.env.PORT || 3000
 
-// middleware
+
+
+// *************middleware************************
 app.use(cors());
 app.use(express.json())
 
 
 
-// curd operation start from here
+
+// *********************mongodb is here************************
+
+const uri = `mongodb+srv://${process.env.GREEN_HUB_USER}:${process.env.GREEN_HUB_PASS}@cluster0.ngwkmsl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+// **************curd operation start from here***********************
 
 app.get('/', (req, res) => {
   res.send('Hello World! this is from green hub')
